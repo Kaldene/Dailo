2c5cec182b6ce8234840f46b81515cf4d857888b
Sonder 2x2 <animesnikanemejt@gmail.com>
2026-04-05 22:31:06 +0300

    сделал обращение к api для базового образение к москве

--- routes/main/main.py
+++ routes/main/main.py
@@ -1,8 +1,34 @@
 from flask import blueprints,render_template
+from datetime import datetime
+from service.weather_service import WeatherService
 
 main_routes = blueprints.Blueprint('main', __name__)
 
 @main_routes.route('/home')
 @main_routes.route('/')
 def home():
-    return render_template('index.html')
+    now=datetime.now()
+
+    months = {
+        1: "Января",
+        2: "февраля",
+        3: "Марта",
+        4: "Апреля",
+        5: "Мая",
+        6: "Июня",
+        7: "Июля",
+        8: "Августа",
+        9: "Сентября",
+        10: "Октября",
+        11: "Ноября",
+        12: "Декабря",
+    }
+
+    weather_service = WeatherService()
+    current_weather_base = weather_service.weather_request_base("Moscow")
+
+    current_date = f"{now.day} {months[now.month]}"
+
+    return render_template('index.html',
+                           current_date=current_date,
+                           current_weather_base=current_weather_base)
--- service/weather_service.py
+++ service/weather_service.py
@@ -3,8 +3,35 @@ from config import Config
 
 
 class WeatherService:
+
     def __init__(self):
         self.api_key = Config.WEATHER_API_KEY
-        self.base_url = 'http://api.weatherapi.com/v1'
+        self.base_url = 'http://api.weatherapi.com/v1/current.json'
 
+    """
+    Получение данных о погоде в москве
+    :return температура в цельсиях, тип погоды 
+    """
+    def weather_request_base(self, city='Moscow'):
+
+        params = {
+            'key': self.api_key,
+            'q': city,
+            'aqi': 'no'
+        }
+
+        try:
+            response = requests.get(self.base_url, params=params)
+            response.raise_for_status()
+
+            data = response.json()
+
+            return (
+                data["current"]["temp_c"],
+                data["current"]["condition"]["text"],
+                data["location "]["name"]
+            )
+        except requests.exceptions.HTTPError as errh:
+            print('Http Error:', errh)
+            return None
 
