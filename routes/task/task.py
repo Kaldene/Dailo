f02798679c48e30e4af9e6a8beb6c5ddded63f9d
Sonder 2x2 <animesnikanemejt@gmail.com>
2026-04-07 12:50:37 +0300

    сделал шаблон для дашборда

--- routes/task/task.py
+++ routes/task/task.py
@@ -5,11 +5,21 @@ from models.task import Task
 
 task_routes = Blueprint('task', __name__)
 
+#задачи, поменять в base.html с дашборда и сделать отдельную функцию отвечающий за это
+
 @task_routes.route('/task')
 @login_required
 def task():
     task = Task.query.filter_by(user_id=current_user.id).order_by(Task.created_at.desc()).all()
     return render_template("task/task.html",task=task)
+
+
+
+@task_routes.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template("task/dashboard.html")
+
 
 @task_routes.route('/create_task',methods=['GET','POST'])
 @login_required
